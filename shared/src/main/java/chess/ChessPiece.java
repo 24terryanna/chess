c91627551b5ea29f961f455d2b570ff0ad7d7389
Anna <annamterry24@gmail.com>
2025-01-22 21:27:20 -0700

    implement get functions, simple returns

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -31,18 +31,18 @@ public class ChessPiece {
     }
 
     /**
-     * @return Which team this chess piece belongs to
+     @return Which team this chess piece belongs to
      */
 
     public ChessGame.TeamColor getTeamColor() {
-        throw new RuntimeException("Not implemented");
+        return teamColor;
     }
 
     /**
      * @return which type of chess piece this piece is
      */
     public PieceType getPieceType() {
-
+        return pieceType;
     }
 
     /**
