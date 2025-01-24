0e4e8f7e6dc898254a445f7a77000d383f742363
Anna <annamterry24@gmail.com>
2025-01-24 16:25:03 -0700

    implement bishop and rook, success

--- shared/src/main/java/chess/QueenMoveCalc.java
+++ shared/src/main/java/chess/QueenMoveCalc.java
@@ -1,17 +1,19 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class QueenMoveCalc implements PieceMovesCalculator {
     @Override
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position) {
-        // Implement the Queen's moves: bishop + rook
-        int queenRow = position.getRow();
-        int queenColumn = position.getColumn();
-        int[][] queenDirection = {{-1, 1}, {0, 1}, {1, 1}, {1, 0}, {1, -1}, {0, -1}, {-1, -1}, {-1, 0}};
+        Collection<ChessMove> queenMoves = new ArrayList<ChessMove>();
 
+        BishopMoveCalc bishopMoves = new BishopMoveCalc();
+        RookMoveCalc rookMoves = new RookMoveCalc();
 
-        return null;
+        queenMoves.addAll(bishopMoves.pieceMoves(board, position));
+        queenMoves.addAll(rookMoves.pieceMoves(board, position));
+
+        return queenMoves;
     }
-    //call rook and bishop; add both to queen collection and return
 }
