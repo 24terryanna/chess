fa97f5bd71eab13fe4f6f2dbd42c29873ff8db8f
Anna <annamterry24@gmail.com>
2025-03-02 00:46:43 -0700

    outline methods

--- server/src/main/java/dataaccess/MemoryUserDAO.java
+++ server/src/main/java/dataaccess/MemoryUserDAO.java
@@ -1,4 +1,33 @@
 package dataaccess;
 
+import model.UserData;
+
+import java.util.HashSet;
+
 public class MemoryUserDAO implements UserDAO{
+    private HashSet<UserData> db;
+
+    public MemoryUserDAO(){
+        db = HashSet.newHashSet(16);
+    }
+
+    @Override
+    public UserData getUser(String username) throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public void createUser(UserData userData) {
+
+    }
+
+    @Override
+    public boolean authUser(String username, String password) throws DataAccessException {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
 }
