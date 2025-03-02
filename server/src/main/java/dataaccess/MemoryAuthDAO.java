d38a6ad33f56e3af0929e88b0849419a70109879
Anna <annamterry24@gmail.com>
2025-03-02 00:56:25 -0700

    created db hashset

--- server/src/main/java/dataaccess/MemoryAuthDAO.java
+++ server/src/main/java/dataaccess/MemoryAuthDAO.java
@@ -1,4 +1,34 @@
 package dataaccess;
 
+import model.AuthData;
+import model.UserData;
+
+import java.util.HashSet;
+
 public class MemoryAuthDAO implements AuthDAO{
+    private HashSet<UserData> db;
+
+    public MemoryAuthDAO(){
+        db = HashSet.newHashSet(16);
+    }
+
+    @Override
+    public void addAuth(AuthData authData) {
+
+    }
+
+    @Override
+    public void deleteAuth(String authToken) {
+
+    }
+
+    @Override
+    public AuthData getAuth(String authToken) throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public void clear() {
+
+    }
 }
--- server/src/main/java/dataaccess/MemoryGameDAO.java
+++ server/src/main/java/dataaccess/MemoryGameDAO.java
@@ -1,4 +1,44 @@
 package dataaccess;
 
+import model.GameData;
+import model.UserData;
+
+import java.util.HashSet;
+
 public class MemoryGameDAO implements GameDAO{
+    private HashSet<UserData> db;
+
+    public MemoryGameDAO(){
+        db = HashSet.newHashSet(16);
+    }
+
+    @Override
+    public HashSet<GameData> listGames() {
+        return null;
+    }
+
+    @Override
+    public void createGame(GameData game) throws DataAccessException {
+
+    }
+
+    @Override
+    public GameData getGame(int gameID) throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public boolean gameExists(int gameID) {
+        return false;
+    }
+
+    @Override
+    public void updateGame(GameData game) throws DataAccessException {
+
+    }
+
+    @Override
+    public void clear() {
+
+    }
 }
