38a600ea22ad0d3ac90452c1ca896716bc83a3d0
Anna <annamterry24@gmail.com>
2025-03-02 23:59:38 -0700

    created db hashmap, basic override written

--- server/src/main/java/dataaccess/MemoryUserDAO.java
+++ server/src/main/java/dataaccess/MemoryUserDAO.java
@@ -2,33 +2,35 @@ package dataaccess;
 
 import model.UserData;
 
-import java.util.HashSet;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MemoryUserDAO implements UserDAO{
-    private HashSet<UserData> db;
+    private Map<String, UserData> db;
 
     public MemoryUserDAO(){
-        db = HashSet.newHashSet(16);
+        db = new HashMap<>();
     }
 
     @Override
     public UserData getUser(String username) throws DataAccessException {
-        return null;
+        return db.get(username);
     }
 
     @Override
     public void createUser(UserData userData) {
-
+        db.put(userData.username(), userData);
     }
 
     @Override
     public boolean authUser(String username, String password) throws DataAccessException {
-        return false;
+        UserData user = db.get(username);
+        return user != null && user.password().equals(password);
     }
 
     @Override
     public void clear() {
-        db = HashSet.newHashSet(16);
+        db.clear();
 
     }
 }
--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -1,7 +1,37 @@
 package service;
 
+import dataaccess.AuthDAO;
+import dataaccess.UserDAO;
+import model.UserData;
+
+import java.util.UUID;
+
 public class UserService {
-    public RegisterResult register(RegisterRequest registerRequest) {}
+    private final UserDAO userDAO;
+    private final AuthDAO authDAO;
+
+    public UserService(UserDAO userDAO, AuthDAO authDAO){
+        this.userDAO = userDAO;
+        this.authDAO = authDAO;
+    }
+
+    public RegisterResult register(RegisterRequest request) throws UserAlreadyExistsException{
+        if (request.getUser() == null || request.getPassword() == null || request.getEmail() == null) {
+            throw new IllegalArgumentException("Invalid request data");
+        }
+
+        if (userDAO.getUser(request.getUser() != null)) {
+            throw new UserAlreadyExistsException("Error: user already taken");
+        }
+
+        UserData newUser = new UserData(request.getUser(), request.getPassward(), request.getEmail());
+        userDAO.createUser(newUser);
+
+        String authToken = UUID.randomUUID().toString();
+        authDAO.storeAuthToken(request.getUser(), authToken);
+
+        return new RegisterResult(request.getUser(), authToken);
+    }
     public LoginResult login(LoginRequest loginRequest) {}
     public void logout(LogoutRequest logoutRequest) {}
 }
