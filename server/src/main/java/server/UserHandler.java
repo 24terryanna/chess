2b311756accb131c1fb37f3ce1b6ebb0bcf7cfd5
Anna <annamterry24@gmail.com>
2025-03-03 13:20:24 -0700

    register, create new user

--- server/src/main/java/server/UserHandler.java
+++ server/src/main/java/server/UserHandler.java
@@ -1,5 +1,41 @@
 package server;
 
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.google.gson.Gson;
+import model.AuthData;
+import org.eclipse.jetty.server.Authentication;
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.Response;
+import service.UserService;
+import model.UserData;
+//import spark.Request;
+//import spark.Response;
+
 public class UserHandler {
+    UserService userService;
+
+    public UserHandler() {
+        this.userService = userService;
+    }
+
+    public Object register(Request request, Response response) throws BadRequestException{
+        UserData userData = new Gson().fromJson(request.body(), UserData.class);
+
+        if (userData.username() == null || userData.password() == null) {
+            throw new BadRequestException("No username or password given");
+        }
+
+        try {
+            AuthData authData = userService.createUser(userData);
+            response.status(200);
+            return new Gson().toJson(authData);
+        } catch (BadRequestException ) {
+            response.status(403);
+            return "{ \"message\": \"Error: already taken\" }";
+        }
+    }
+
+
 
 }
--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -1,7 +1,9 @@
 package service;
 
 import dataaccess.AuthDAO;
+import dataaccess.DataAccessException;
 import dataaccess.UserDAO;
+import model.AuthData;
 import model.UserData;
 
 import java.util.UUID;
@@ -15,23 +17,44 @@ public class UserService {
         this.authDAO = authDAO;
     }
 
-    public RegisterResult register(RegisterRequest request) throws UserAlreadyExistsException{
-        if (request.getUser() == null || request.getPassword() == null || request.getEmail() == null) {
-            throw new IllegalArgumentException("Invalid request data");
+    public RegisterResult register(RegisterRequest request) {
+        if (request.username() == null || request.password() == null || request.email() == null) {
+            return new RegisterResult("Error: bad request", 400);
         }
 
-        if (userDAO.getUser(request.getUser() != null)) {
-            throw new UserAlreadyExistsException("Error: user already taken");
+        if (userDAO.getUser(request.username() != null)) {
+            return new RegisterResult("Error: already taken", 403);
         }
 
-        UserData newUser = new UserData(request.getUser(), request.getPassward(), request.getEmail());
+        //create the new user
+        UserData newUser = new UserData(request.username(), request.password(), request.email());
         userDAO.createUser(newUser);
 
+        //create authtoken for new user
         String authToken = UUID.randomUUID().toString();
-        authDAO.storeAuthToken(request.getUser(), authToken);
+        AuthData authData = new AuthData(request.username(), authToken);
+        authDAO.createAuth(authData);
 
-        return new RegisterResult(request.getUser(), authToken);
+        return new RegisterResult(request.username(), authToken, 200);
     }
-    public LoginResult login(LoginRequest loginRequest) {}
-    public void logout(LogoutRequest logoutRequest) {}
+//    public LoginResult login(LoginRequest loginRequest) {}
+//    public void logout(LogoutRequest logoutRequest) {}
 }
+
+
+
+//    public AuthData createUser(UserData userData) throws BadRequestException { //need to create BadRequest
+//        try {
+//            userDAO.createUser((userData));
+//        } catch (DataAccessException e) {
+//            throw new BadRequestException(e.getMessage());
+//        }
+//        String authToken = UUID.randomUUID().toString();
+//        AuthData authData = new AuthData(userData.username(), authToken);
+//        authDAO.createAuth(authData);
+//
+//        return authData;
+//    }
+
+
+
