c62ff442f53e99692a6d5c18fd600cb6925f961a
Anna <annamterry24@gmail.com>
2025-03-04 11:06:06 -0700

    updated login with result and response route

--- server/src/main/java/server/UserHandler.java
+++ server/src/main/java/server/UserHandler.java
@@ -4,6 +4,9 @@ import com.google.gson.Gson;
 import dataaccess.DataAccessException;
 import model.UserData;
 import model.RegisterResult;
+import model.RegisterRequest;
+import model.LoginResult;
+import model.LoginRequest;
 import service.UserService;
 import spark.Request;
 import spark.Response;
@@ -20,9 +23,9 @@ public class UserHandler {
 
     public Route registerUser = (Request req, Response res) -> {
         try {
-            UserData.RegisterRequest request = gson.fromJson(req.body(), UserData.RegisterRequest.class);
+            RegisterRequest request = gson.fromJson(req.body(), RegisterRequest.class);
 
-            if (request.getUsername() == null || request.getPassword() == null || request.getEmail() == null) {
+            if (request.username() == null || request.password() == null || request.email() == null) {
                 res.status(400);
                 return gson.toJson(new RegisterResult("Error: bad request", 400));
             }
@@ -37,11 +40,22 @@ public class UserHandler {
         }
     };
 
-    public Route login(Request request, Response response) {
+    public Route login = (Request req, Response res) -> {
         try {
+            LoginRequest request = gson.fromJson(req.body(), LoginRequest.class);
 
-        } catch() {
+            if (request.username() == null || request.password() == null) {
+                res.status(400);
+                return gson.toJson(new LoginResult("Error: bad request", 400));
+            }
 
+            LoginResult result = userService.login(request);
+            res.status(result.statusCode());
+            return gson.toJson(result);
+
+        } catch(DataAccessException e) {
+            res.status(500);
+            return gson.toJson(new LoginResult("ErrorL " + e.getMessage(), 500));
         }
-    }
+    };
 }
