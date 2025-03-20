dafa2e83cbc11e231d649c54b8ff1ed6fa630742
Anna <annamterry24@gmail.com>
2025-03-19 22:24:38 -0600

    create auth and get auth implemented

--- server/src/main/java/dataaccess/SqlAuthDAO.java
+++ server/src/main/java/dataaccess/SqlAuthDAO.java
@@ -2,14 +2,47 @@ package dataaccess;
 
 import model.AuthData;
 
-public class SqlAuthDAO implements AuthDAO{
-    @Override
-    public void createAuth(AuthData authData) {
+import java.sql.*;
 
+public class SqlAuthDAO implements AuthDAO{
+
+    public SqlAuthDAO() throws DataAccessException {
+        configureDatabase();
+    }
+
+    @Override
+    public void createAuth(AuthData authData) throws DataAccessException {
+        var statement = "INSERT INTO auth (auth_token, username) VALUES (?, ?)";
+        try (var conn = DatabaseManager.getConnection();
+             var ps = conn.prepareStatement(statement)) {
+
+            ps.setString(1, authData.authToken());
+            ps.setString(2, authData.username());
+            ps.executeUpdate();
+
+        } catch (SQLException | DataAccessException e) {
+            throw new DataAccessException("Error creating auth token: " + e.getMessage());
+        }
     }
 
     @Override
     public AuthData getAuth(String authToken) throws DataAccessException {
+        var statement = "SELECT auth_token, username FROM auth WHERE auth_token=?";
+        try (var conn = DatabaseManager.getConnection();
+             var ps = conn.prepareStatement(statement)) {
+
+            ps.setString(1, authToken);
+            try (var rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    return new AuthData(
+                            rs.getString("auth_token"),
+                            rs.getString("username")
+                    );
+                }
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException("Error retrieving auth token: " + e.getMessage());
+        }
         return null;
     }
 
