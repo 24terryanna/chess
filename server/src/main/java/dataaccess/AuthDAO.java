5fd3db9475b1ba1eba5a50f4aa101b61bcbcc974
Anna <annamterry24@gmail.com>
2025-03-19 22:28:14 -0600

    delete, clear, configure toauth

--- server/src/main/java/dataaccess/AuthDAO.java
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -4,6 +4,6 @@ import model.AuthData;
 public interface AuthDAO {
     void createAuth(AuthData authData) throws DataAccessException;
     AuthData getAuth(String authToken) throws DataAccessException;
-    void deleteAuth(String authToken);
+    void deleteAuth(String authToken) throws DataAccessException;
     void clear();
 }
--- server/src/main/java/dataaccess/SqlAuthDAO.java
+++ server/src/main/java/dataaccess/SqlAuthDAO.java
@@ -4,9 +4,9 @@ import model.AuthData;
 
 import java.sql.*;
 
-public class SqlAuthDAO implements AuthDAO{
+public class SqlAuthDAO implements AuthDAO {
 
-    public SqlAuthDAO() throws DataAccessException {
+    public SqlAuthDAO() throws DataAccessException, SQLException {
         configureDatabase();
     }
 
@@ -47,12 +47,46 @@ public class SqlAuthDAO implements AuthDAO{
     }
 
     @Override
-    public void deleteAuth(String authToken) {
+    public void deleteAuth(String authToken) throws DataAccessException {
+        var statement = "DELETE FROM auth WHERE auth_token=?";
+        try (var conn = DatabaseManager.getConnection();
+             var ps = conn.prepareStatement(statement)) {
 
+            ps.setString(1, authToken);
+            ps.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new DataAccessException("Error deleting auth token: " + e.getMessage());
+        }
     }
 
     @Override
     public void clear() {
+        var statement = "TRUNCATE TABLE auth";
+        try (var conn = DatabaseManager.getConnection();
+             var ps = conn.prepareStatement(statement)) {
+            ps.executeUpdate();
+        } catch (SQLException | DataAccessException e) {
+            throw new RuntimeException("Error clearing auth table: " + e.getMessage(), e);
+        }
+    }
 
+    private void configureDatabase() throws DataAccessException, SQLException {
+        DatabaseManager.createDatabase();
+        try (var conn = DatabaseManager.getConnection()) {
+            var createTable = """
+                CREATE TABLE IF NOT EXISTS auth (
+                    auth_token VARCHAR(255) NOT NULL,
+                    username VARCHAR(255) NOT NULL,
+                    PRIMARY KEY (auth_token),
+                    FOREIGN KEY (username) REFERENCES user(username) ON DELETE CASCADE
+                ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            """;
+            try (var ps = conn.prepareStatement(createTable)) {
+                ps.executeUpdate();
+            }
+        } catch (SQLException | DataAccessException e) {
+            throw new RuntimeException("Unable to configure database: " + e.getMessage(), e);
+        }
     }
 }
