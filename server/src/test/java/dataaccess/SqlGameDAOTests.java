d13dddc2d14bee96998aa28856436402e5f1a8c2
Anna <annamterry24@gmail.com>
2025-03-22 20:43:21 -0600

    game tests

--- server/src/test/java/dataaccess/SqlGameDAOTests.java
+++ server/src/test/java/dataaccess/SqlGameDAOTests.java
@@ -5,8 +5,13 @@ import chess.ChessGame;
 import model.GameData;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class SqlGameDAOTests {
 
@@ -38,4 +43,52 @@ public class SqlGameDAOTests {
             }
         }
     }
+
+    @Test
+    void testCreateGame() throws DataAccessException {
+        gameDAO.createGame(defaultGameData);
+        GameData retrievedGame = gameDAO.getGame(defaultGameData.gameID());
+
+        assertNotNull(retrievedGame);
+        assertEquals(defaultGameData.gameID(), retrievedGame.gameID());
+        assertEquals(defaultGameData.whiteUsername(), retrievedGame.whiteUsername());
+        assertEquals(defaultGameData.blackUsername(), retrievedGame.blackUsername());
+        assertEquals(defaultGameData.gameName(), retrievedGame.gameName());
+    }
+
+    @Test
+    void testGetGame_NotFound() {
+        assertThrows(DataAccessException.class, () -> gameDAO.getGame(9999)); // Random non-existent ID
+    }
+
+    @Test
+    void testUpdateGame() throws DataAccessException {
+        gameDAO.createGame(defaultGameData);
+        GameData updatedGame = new GameData(1111, "newWhite", "newBlack", "newGameName", defaultGameData.game());
+        gameDAO.updateGame(updatedGame);
+
+        GameData retrievedGame = gameDAO.getGame(1111);
+        assertEquals("newWhite", retrievedGame.whiteUsername());
+        assertEquals("newBlack", retrievedGame.blackUsername());
+        assertEquals("newGameName", retrievedGame.gameName());
+    }
+
+    @Test
+    void testListGames() throws DataAccessException {
+        gameDAO.createGame(defaultGameData);
+        List<GameData> games = gameDAO.listGames("white");
+
+        assertEquals(1, games.size());
+        assertEquals(defaultGameData.gameID(), games.get(0).gameID());
+    }
+
+    @Test
+    void testClear() throws DataAccessException {
+        gameDAO.createGame(defaultGameData);
+        gameDAO.clear();
+
+        assertThrows(DataAccessException.class, () -> gameDAO.getGame(defaultGameData.gameID()));
+    }
+
+
 }
