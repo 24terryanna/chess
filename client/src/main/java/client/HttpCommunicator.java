9dda27676f95c07a37142e0bd2762c146d1789dd
Anna <annamterry24@gmail.com>
2025-04-05 22:41:29 -0600

    outline httpcomm

--- client/src/main/java/client/HttpCommunicator.java
+++ client/src/main/java/client/HttpCommunicator.java
@@ -1,4 +1,59 @@
 package client;
 
+import com.google.gson.Gson;
+import model.GameData;
+import server.ServerFacade;
+
+import java.util.List;
+import java.util.Map;
+
 public class HttpCommunicator {
+
+    String url;
+    ServerFacade serverFacade;
+
+    public HttpCommunicator(ServerFacade serverFacade, String serverDomain) {
+        url = "http://" + serverDomain;
+        this.serverFacade = serverFacade;
+    }
+
+    public boolean register(String username, String password, String email) {
+        var body = Map.of("username", username, "password", password, "email", email);
+        var jsonBody = new Gson().toJson(body);
+        Map response = request("POST", "/user", jsonBody);
+        if (response.containsKey("Error")) {
+            return false;
+        }
+        serverFacade.setAuthToken((String) response.get("authToken"));
+        return true;
+    }
+
+    public boolean login(String username, String password){
+
+    }
+
+    public boolean logout() {
+
+    }
+
+    public int createGame(String gameName) {
+
+    }
+
+    public List<GameData> listGames() {
+
+    }
+
+    public boolean joinGame(int gameID, String playerColor) {
+
+    }
+
+    private Map request(String method, String endpoint) {
+        return request(method, endpoint);
+    }
+
+    private Map request(String method, String endpoint, String body) {
+
+    }
+
 }
