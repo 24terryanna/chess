a235f2eab12af81420182d0d94b92ced0822514e
Anna <annamterry24@gmail.com>
2025-04-07 18:48:55 -0600

    change getSymbol logic to switch case

--- client/src/main/java/ui/BoardPrinter.java
+++ client/src/main/java/ui/BoardPrinter.java
@@ -71,13 +71,31 @@ public class BoardPrinter {
         ChessPiece.PieceType type = piece.getPieceType();
 
         if (color == ChessGame.TeamColor.WHITE) {
-            if (type == ChessPiece.PieceType.KING) return WHITE_KING;
-            if (type == ChessPiece.PieceType.QUEEN) return WHITE_QUEEN;
-            if (type == ChessPiece.PieceType.BISHOP) return WHITE_BISHOP;
-            if (type == ChessPiece.PieceType.KNIGHT) return WHITE_KNIGHT;
-            if (type == ChessPiece.PieceType.ROOK) return WHITE_ROOK;
-            if (type == ChessPiece.PieceType.PAWN) return WHITE_PAWN;
+            return switch (piece.getPieceType()) {
+                case KING -> piece.getTeamColor() == ChessGame.TeamColor.WHITE ? WHITE_KING : BLACK_KING;
+                case QUEEN -> piece.getTeamColor() == ChessGame.TeamColor.WHITE ? WHITE_QUEEN : BLACK_QUEEN;
+                case ROOK -> piece.getTeamColor() == ChessGame.TeamColor.WHITE ? WHITE_ROOK : BLACK_ROOK;
+                case BISHOP -> piece.getTeamColor() == ChessGame.TeamColor.WHITE ? WHITE_BISHOP : BLACK_BISHOP;
+                case KNIGHT -> piece.getTeamColor() == ChessGame.TeamColor.WHITE ? WHITE_KNIGHT : BLACK_KNIGHT;
+                case PAWN -> piece.getTeamColor() == ChessGame.TeamColor.WHITE ? WHITE_PAWN : BLACK_PAWN;
+            };
         }
+//            if (type == ChessPiece.PieceType.KING) return WHITE_KING;
+//            if (type == ChessPiece.PieceType.QUEEN) return WHITE_QUEEN;
+//            if (type == ChessPiece.PieceType.BISHOP) return WHITE_BISHOP;
+//            if (type == ChessPiece.PieceType.KNIGHT) return WHITE_KNIGHT;
+//            if (type == ChessPiece.PieceType.ROOK) return WHITE_ROOK;
+//            if (type == ChessPiece.PieceType.PAWN) return WHITE_PAWN;
+//        } else {
+//            if (type == ChessPiece.PieceType.KING) return BLACK_KING;
+//            if (type == ChessPiece.PieceType.QUEEN) return BLACK_QUEEN;
+//            if (type == ChessPiece.PieceType.BISHOP) return BLACK_BISHOP;
+//            if (type == ChessPiece.PieceType.KNIGHT) return BLACK_KNIGHT;
+//            if (type == ChessPiece.PieceType.ROOK) return BLACK_ROOK;
+//            if (type == ChessPiece.PieceType.PAWN) return BLACK_PAWN;
+//        }
+//
+        return EMPTY;
     }
 
 
