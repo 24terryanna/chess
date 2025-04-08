6ebdc18e4bd1ba7eebe02ef976c80118e42accab
Anna <annamterry24@gmail.com>
2025-04-07 18:33:23 -0600

    set checkered bg & text color

--- client/src/main/java/ui/BoardPrinter.java
+++ client/src/main/java/ui/BoardPrinter.java
@@ -2,6 +2,8 @@ package ui;
 
 import chess.ChessBoard;
 import chess.ChessGame;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 import static ui.EscapeSequences.*;
 
@@ -28,6 +30,33 @@ public class BoardPrinter {
             files = new char[]{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
         } else {
             files = new char[]{'h', 'g', 'f', 'e', 'd', 'c', 'b', 'a'};
+        }
+
+        for (int row = startRow; row != endRow; row += step) {
+            System.out.print(" " + row + " ");
+            for (char col : files) {
+                int colNum = col - 'a' + 1;
+                ChessPosition position = new ChessPosition(row, colNum);
+                ChessPiece piece = board.getPiece(position);
+
+                boolean isLightSquare = (row + colNum) % 2 == 0;
+                String bgColor;
+                if (isLightSquare) {
+                    bgColor = SET_BG_COLOR_LIGHT_GREY;
+                } else {
+                    bgColor = SET_BG_COLOR_DARK_GREEN;
+                }
+
+                String display;
+                if (piece == null) {
+                    display = EMPTY;
+                } else {
+                    display = getSymbol(piece);
+                }
+
+                System.out.print(bgColor + display + RESET_BG_COLOR + RESET_TEXT_COLOR);
+            }
+            System.out.println();
         }
     }
 
