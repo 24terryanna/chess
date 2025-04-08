6a8e00272bd3fc2b19ac0cbf83127b91d2a52bdf
Anna <annamterry24@gmail.com>
2025-04-07 18:28:35 -0600

    board printer, initialize perspective

--- client/src/main/java/ui/BoardPrinter.java
+++ client/src/main/java/ui/BoardPrinter.java
@@ -1,4 +1,35 @@
 package ui;
 
+import chess.ChessBoard;
+import chess.ChessGame;
+
+import static ui.EscapeSequences.*;
+
 public class BoardPrinter {
+    public void printBoard(ChessBoard board, ChessGame.TeamColor perspective) {
+        System.out.println(ERASE_SCREEN);
+
+        int startRow;
+        int endRow;
+        int step;
+
+        if  (perspective == ChessGame.TeamColor.WHITE) {
+            startRow = 8;
+            endRow = 0;
+            step = -1;
+        } else {
+            startRow = 1;
+            endRow = 9;
+            step = 1;
+        }
+
+        char[] files;
+        if (perspective == ChessGame.TeamColor.WHITE) {
+            files = new char[]{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
+        } else {
+            files = new char[]{'h', 'g', 'f', 'e', 'd', 'c', 'b', 'a'};
+        }
+    }
+
+
 }
